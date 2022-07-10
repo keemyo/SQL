a90e39da8a55074deaf7d80bae48a815b13dcee4
creactive0510@gmail.com <creactive0510@gmail.com>
2022-07-10 21:38:41 +0900

    0710_sql_study

--- Chapter5. 테이블 만들기.sql
+++ Chapter5. 테이블 만들기.sql
@@ -58,6 +58,35 @@ height TINYINT UNSIGNED NULL,
 debut_date DATE NULL
 );
 
+-- 5) buy 테이블 만들기 
+DROP TABLE IF EXISTS buy; -- 기존에 있으면 삭제 
+CREATE TABLE buy -- 구매 테이블 
+( num INT AUTO_INCREMENT NOT NULL PRIMARY KEY, -- 순번(PK) 
+mem_id CHAR(8) NOT NULL, -- 아이디(FK)
+prod_name CHAR(6) NOT NULL, -- 제품 이름 
+group_name CHAR(4) NULL, -- 분류
+price INT UNSIGNED NOT NULL, -- 가격 
+amount  SMALLINT UNSIGNED NOT NULL -- 수량 
+); 
 
+-- 6) buy: Foeign target 
+DROP TABLE IF EXISTS buy; -- 기존에 있으면 삭제 
+CREATE TABLE buy 
+( num INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+mem_id CHAR(8) NOT NULL,
+prod_name CHAR(6) NOT NULL, 
+group_name CHAR(4) NULL, 
+price INT UNSIGNED NOT NULL,
+amount SMALLINT UNSIGNED NOT NULL, 
+FOREIGN KEY(mem_id) REFERENCES member(mem_id)
+) ; 
 
+-- 7) 데이터 입력하기 
+INSERT INTO member VALUES('TWC', '트와이스', 9, '서울', '02', '11111111', 167, '2015-10-19');
+INSERT INTO member VALUES('BLK', '블랙핑크', 4, '경남', '055','22222222', 173, '2016-8-8');
+INSERT INTO member VALUES('WMW', '여자친구', '경기', '031', '33333333', 166, '2015-1-15');
+
+INSERT INTO buy VALUES(NULL, 'BLK', '지갑', NULL, 30, 2);
+INSERT INTO buy VALUES(NULL, 'BLK', '맥북프로','디지털', 1000, 1);
+INSERT INTO buy VALUES(NULL, 'APN', '아이폰', '디지털', 200, 1);
 
